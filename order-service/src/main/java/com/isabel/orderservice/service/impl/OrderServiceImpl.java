d0c5ae8ab65b085a8defc8a00a0ce7e0d277d136
IsabelNzioka <isabelnzioka@gmail.com>
2024-03-25 04:32:14 +0300

    Register Clients to eureka
    - added load balanced configuration to the service for webclient builder

--- order-service/src/main/java/com/isabel/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/isabel/orderservice/service/impl/OrderServiceImpl.java
@@ -32,11 +32,11 @@ import reactor.core.publisher.Mono;
 public class OrderServiceImpl implements OrderService{
 
    private final OrderRepository orderRepository;
-   private final WebClient webClient;
+   private final WebClient.Builder webClientBuilder;
    
-   public OrderServiceImpl(OrderRepository orderRepository, WebClient webClient) {
+   public OrderServiceImpl(OrderRepository orderRepository, WebClient.Builder webClientBuilder) {
       this.orderRepository = orderRepository;
-      this.webClient = webClient;
+      this.webClientBuilder = webClientBuilder;
    }
 
     @Override
@@ -57,8 +57,8 @@ public class OrderServiceImpl implements OrderService{
         }
         log.info("{}", productCodes);
         log.info("{}", productQuantities);
-        GenericResponse<?> response = webClient.get()
-                .uri("http://localhost:6002/api/inventory/check",
+        GenericResponse<?> response = webClientBuilder.build().get()
+                .uri("http://INVENTORY-SERVICE/api/inventory/check",
                         uriBuilder -> uriBuilder
                                 .queryParam("productCodes", productCodes)
                                 .queryParam("productQuantities", productQuantities)
