8dda76c5132bbbe455fa4fcbd9048634337599aa
IsabelNzioka <isabelnzioka@gmail.com>
2024-03-27 06:14:43 +0300

    Add zipkin tracing, Adjust config centralization

--- inventory/src/main/java/com/isabel/inventory/entity/Inventory.java
+++ inventory/src/main/java/com/isabel/inventory/entity/Inventory.java
@@ -1,6 +1,7 @@
 package com.isabel.inventory.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ public class Inventory {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String productCode;
 
     private Integer quantity;
